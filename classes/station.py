9575f9414a5444e1c5c64323083a80edd1d39bc9
Berto Ferrero <berto@prestaquality.com>
2024-05-29 18:40:01 +0200

    Bucle principal

---
+++ classes/station.py
@@ -0,0 +1,75 @@
+
+class Station:
+    '''
+    Class representing an access point station.
+    '''
+    def __init__(self, mac, x, y, frequency, initial_timestamp=0):
+        """
+        Constructor for the Station class.
+
+        Args:
+            mac (str): The MAC address of the access point station.
+            x (float): The x-coordinate of the access point station's location.
+            y (float): The y-coordinate of the access point station's location.
+            frequency (int): The transmission frequency of the access point station in milliseconds.
+            initial_timestamp (int, optional): The initial timestamp of the access point station. Defaults to 0.
+        """
+        self._mac = mac
+        self._x = x
+        self._y = y
+        self._frequency = frequency
+        self._next_transmission_timestamp = 0
+        self._last_transmission_timestamp = initial_timestamp
+
+    @property
+    def mac(self):
+        """
+        str: The MAC address of the access point station.
+        """
+        return self._mac
+
+    @property
+    def x(self):
+        """
+        float: The x-coordinate of the access point station's location.
+        """
+        return self._x
+
+    @property
+    def y(self):
+        """
+        float: The y-coordinate of the access point station's location.
+        """
+        return self._y
+
+    @property
+    def frequency(self):
+        """
+        int: The transmission frequency of the access point station in milliseconds.
+        """
+        return self._frequency
+
+    @property
+    def last_transmission_timestamp(self):
+        """
+        int: The timestamp of the last transmission made by the access point station.
+        """
+        return self._last_transmission_timestamp
+
+    @last_transmission_timestamp.setter
+    def last_transmission_timestamp(self, timestamp):
+        """
+        Setter for the last transmission timestamp.
+
+        Args:
+            timestamp (int): The timestamp of the last transmission made by the access point station.
+        """
+        self._last_transmission_timestamp = timestamp
+        self._next_transmission_timestamp = self._last_transmission_timestamp + self._frequency
+
+    @property
+    def next_transmission_timestamp(self):
+        """
+        int: The timestamp of the next scheduled transmission by the access point station.
+        """
+        return self._next_transmission_timestamp
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import argparse
 import os
 import json
+from classes.station import Station
 
 class App:
-    def __init__(self, config_path):
+    def __init__(self, config_path, stations_path):
+        # Load settings and stations
         self.loadSettings(config_path=config_path)
+        self.loadStations(stations_path=stations_path)
 
     def loadSettings(self, config_path):
         # First check if the config file exists
@@ -19,17 +22,82 @@ class App:
         if not isinstance(self.config, dict):
             raise ValueError("Invalid config file format.")
 
+        # Verify coherence
+        if self.config['initial_position']['x'] < self.config['initial_position']['margin_meters'] or self.config['initial_position']['x'] > self.config['initial_position']['x'] - self.config['initial_position']['margin_meters']:
+            raise ValueError("Initial x position is out of bounds.")
+        if self.config['initial_position']['y'] < self.config['initial_position']['margin_meters'] or self.config['initial_position']['y'] > self.config['initial_position']['y'] - self.config['initial_position']['margin_meters']:
+            raise ValueError("Initial y position is out of bounds.")
+    
+    def loadStations(self, stations_path):
+        # First check if the config file exists
+        if not os.path.exists(stations_path):
+            raise FileNotFoundError(f"Stations definition file {stations_path} not found")
+
+        # Load the config file as json file
+        with open(stations_path, 'r') as file:
+            stationsConfig = json.load(file)
+
+        # Check if setationsConfig is a list
+        if not isinstance(stationsConfig, list):
+            raise ValueError("Invalid stations definition file format.")
+
+        # Lets go, load all the stations
+        self.stations = []
+        for station in stationsConfig:
+            # Check if all the required fields are present
+            if not all([field in station for field in ['mac', 'x', 'y', 'frequency']]):
+                raise ValueError("Invalid station definition format.")
+            self.stations.append(Station(**station))
+        
+
     def start(self):
-        # Aquí es donde colocarías la lógica para iniciar tu aplicación
+        # Initialize the simulation
+        max_time_milliseconds = self.config['simulation_duration_seconds'] * 1000
+        current_time = 0
+        iteration = 0
+        milliseconds_per_iteration = min([station.frequency for station in self.stations])
+        if milliseconds_per_iteration < 10:
+            raise ValueError("Minimum frequency is 10 millisecond.")
+        pos_x = self.config['initial_position']['x']
+        pos_y = self.config['initial_position']['y']
+
+        # Define maximal and minimal x and y coordinates
+        min_x = self.config['initial_position']['margin_meters']
+        max_x = self.config['initial_position']['x'] - self.config['initial_position']['margin_meters']
+        min_y = self.config['initial_position']['margin_meters']
+        max_y = self.config['initial_position']['y'] - self.config['initial_position']['margin_meters']
+
+        # Main loop
+        while True:
+            # Increase counters
+            iteration += 1
+            current_time += milliseconds_per_iteration
+    
+            # Calculate the new position of the stations
+            # TODO: Implement the logic to calculate the new position of the stations
+            pos_x = random.uniform(min_x, max_x)
+            pos_y = random.uniform(min_y, max_y)
+
+            # For each required station, generate the static RSSI value
+            for station in [station for station in self.stations if station.next_transmission_timestamp <= current_time]:
+                # TODO: Implement the logic to calculate the RSSI value
+                rssi = random.randint(-100, 0)
+                # Set the last transmission timestamp
+                station.last_transmission_timestamp = current_time
+
+            # Check we achieved the maximum execution time
+            if self.current_time >= max_time_milliseconds:
+                break
         pass
 
 def main():
     #Load arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', default=os.path.join(os.path.dirname(__file__), 'config.json'))
+    parser.add_argument('--stations', default=os.path.join(os.path.dirname(__file__), 'stations.json'))
     args = parser.parse_args()
 
-    app = App(args.config)
+    app = App(args.config, args.stations)
     app.start()
 
 if __name__ == "__main__":
