1847a23dbb94cc15a2c55f92149d42fc7bb3ba87
Berto Ferrero <berto@prestaquality.com>
2024-05-30 09:35:38 +0200

    Valores A1 y A2 añadidos

--- classes/models/station.py
+++ classes/models/station.py
@@ -3,7 +3,8 @@ class Station:
     '''
     Class representing an access point station.
     '''
-    def __init__(self, mac, x, y, frequency, initial_timestamp=0):
+
+    def __init__(self, mac: str, x: float, y: float, frequency: int, A1: float = None, A2: float = None, initial_timestamp: int = 0):
         """
         Constructor for the Station class.
 
@@ -12,12 +13,16 @@ class Station:
             x (float): The x-coordinate of the access point station's location.
             y (float): The y-coordinate of the access point station's location.
             frequency (int): The transmission frequency of the access point station in milliseconds.
+            A1 (float, optional): The A1 parameter of the access point station, used for the RSSI to Distance formula. Defaults to None.
+            A2 (float, optional): The A2 parameter of the access point station, used for the RSSI to Distance formula. Defaults to None.
             initial_timestamp (int, optional): The initial timestamp of the access point station. Defaults to 0.
         """
         self._mac = mac
         self._x = x
         self._y = y
         self._frequency = frequency
+        self._A1 = A1
+        self._A2 = A2
         self._last_transmission_timestamp = initial_timestamp
         self._next_transmission_timestamp = initial_timestamp + frequency
 
@@ -48,6 +53,20 @@ class Station:
         int: The transmission frequency of the access point station in milliseconds.
         """
         return self._frequency
+
+    @property
+    def A1(self) -> float:
+        """
+        float: The A1 parameter of the access point station.
+        """
+        return self._A1
+
+    @property
+    def A2(self) -> float:
+        """
+        float: The A2 parameter of the access point station.
+        """
+        return self._A2
 
     @property
     def last_transmission_timestamp(self) -> int:
